f94cf0773cda13c7b602ec6254ea73174f26f607
Sandeep Jaiswal <sandeepjswl123@gmail.com>
2018-06-12 09:41:30 +0530

    Add assignments

---
+++ Capstone: Retrieving, Processing, and Visualizing Data with Python/Assignments/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','uct.ac.za','columbia.edu','indiana.edu','mac.com','berkeley.edu','unl.edu','mtu.edu','ufp.pt'],
+['2005-12',57,11,14,13,12,12,12,12,11,10],
+['2006-01',0,4,0,0,1,0,0,0,0,1]
+];
